ecc1b129f8b0383972caf9426c6892d862992281
Joao Paulo <jpautran1414@gmail.com>
2020-07-09 13:46:33 -0300

    db i

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,5 +1,7 @@
 const { age, date } = require('../../lib/utils');
 const intl = require('intl');
+const { DateTimeFormat } = require('intl');
+const db = require('../../config/db');
 
 module.exports = {
     index(req, res) {
@@ -17,9 +19,32 @@ module.exports = {
             }
         }
 
-        let { avatar_url, birth, name, services, gender } = req.body;
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
 
-        return
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).iso,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, function(err, results) {
+            if(err) return res.send("Database Error!");
+            return res.redirect(`/instructors/${results.row[0].id}`);
+        });
+
     },
     show(req, res) {
         return
